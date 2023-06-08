2082169d9b613e032120d86c3ab03231f9eecf55
hykepashi <adrian.hoff2005@gmail.com>
2023-06-07 22:17:32 -0300

    feat: tesxts/ create this obj

---
+++ src/content/TEXTS.ts
@@ -0,0 +1,21 @@
+export const TEXTS = {
+  signInScreen: {
+    dashboardHeaderComponent: {
+      text_1: "taskly",
+      text_2: "your life with us",
+    },
+    inputComponent: {
+      text_1: "email",
+      text_2: "password",
+    },
+    textComponent: {
+      text_1: "or",
+      text_2: "Forgot your password?",
+      text_3: " Recover password",
+    },
+    touchableOpacityComponent: {
+      text_1: "sign in",
+      text_2: "create account",
+    },
+  },
+};
