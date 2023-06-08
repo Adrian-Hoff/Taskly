3c96e3e835ff17cda868b53ae051be242d570808
hykepashi <adrian.hoff2005@gmail.com>
2023-06-07 22:53:09 -0300

    feat: App/ add font and splash rendering while fonts loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,37 @@
+//react
+import { useCallback } from "react";
+
+//expo
+import {
+  useFonts,
+  Lato_300Light,
+  Lato_400Regular,
+  Lato_700Bold,
+} from "@expo-google-fonts/lato";
+import * as SplashScreen from "expo-splash-screen";
+
 //native-base
 import { Box, NativeBaseProvider, StatusBar } from "native-base";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_700Bold,
+  });
+
+  useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NativeBaseProvider>
       <Box flex={1}>
