a2457f2a86dd45c0fc94e9dd7ef9b58aeabde609
hykepashi <adrian.hoff2005@gmail.com>
2023-06-08 20:25:00 -0300

    refactor: TypeTouchableOpacityComponent/ refactor this type

--- src/types/TypeTouchableOpacityComponent.ts
+++ src/types/TypeTouchableOpacityComponent.ts
@@ -1,7 +1,15 @@
+//react-native
 import { TouchableOpacityProps } from "react-native";
 
-export type TypeTouchableOpacityComponent = TouchableOpacityProps & {
-  bg: string;
-  text: string;
-  color: string;
-};
+//native-base
+import { ITextProps } from "native-base";
+
+export type TypeTouchableOpacityComponent = ITextProps &
+  TouchableOpacityProps & {
+    bg: string;
+    text: string;
+    color: string;
+    google: boolean;
+    fontSize: string;
+    fontFamily: string;
+  };
