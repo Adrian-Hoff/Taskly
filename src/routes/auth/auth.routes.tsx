d53015e23e13ec7fe607cd9f2a70a903523afe81
hykepashi <adrian.hoff2005@gmail.com>
2023-06-08 22:55:51 -0300

    refactor: AuthRoutes/ use TypeAuthRoutes

--- src/routes/auth/auth.routes.tsx
+++ src/routes/auth/auth.routes.tsx
@@ -3,9 +3,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignInScreen from "../../screens/auth/SignInScreen";
 import { THEMES } from "../../themes/Themes";
 import SignUpScreen from "../../screens/auth/SignUpScreen";
+import { TypeAuthRoutes } from "../../types/@routes/auth/TypeAuthRoutes";
 
 export default function AuthRoutes() {
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator<TypeAuthRoutes>();
 
   return (
     <Stack.Navigator
