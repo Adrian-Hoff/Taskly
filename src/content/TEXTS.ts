a7f6fdd2dc52d7ea754877e97d7611cd352e6042
hykepashi <adrian.hoff2005@gmail.com>
2023-06-08 22:17:34 -0300

    feat: TEXTS/ add signUpScreen texts

--- src/content/TEXTS.ts
+++ src/content/TEXTS.ts
@@ -19,4 +19,26 @@ export const TEXTS = {
       text_3: "create account",
     },
   },
+  signUpScreen: {
+    stackHeaderComponent: {
+      text_1: "back",
+      text_2: "create account",
+    },
+    inputComponent: {
+      text_1: "Email",
+      text_2: "Password",
+      text_3: "Confirm Password",
+      text_4: "phone number",
+    },
+    textComponent: {
+      text_1: "By creating your Taskly account, you are agreeing to our",
+      text_2: "terms of use",
+      text_3: "and",
+      text_4: "privacy policy.",
+      text_5: "Please make sure to read them carefully before proceeding.",
+    },
+    touchableOpacityComponent: {
+      text_1: "create account",
+    },
+  },
 };
