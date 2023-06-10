4979bb84222faac2b9c34a20d50991bdbd38ec93
hykepashi <adrian.hoff2005@gmail.com>
2023-06-10 13:14:50 -0300

    feat: TypeFirebaseCreateUserWithEmailAndPassword/ add toast (custom hook) function type

--- src/types/@firebase/TypeFirebaseCreateUserWithEmailAndPassword.tsx
+++ src/types/@firebase/TypeFirebaseCreateUserWithEmailAndPassword.tsx
@@ -1,6 +1,9 @@
+import { TypeUseToast } from "../@hooks/TypeUseToast/TypeUseToast";
+
 export type TypeFirebaseCreateUserWithEmailAndPassword = {
   name: string;
   email: string;
   password: string;
   passwordConfirmation: string;
+  toast: ({ message, type }: TypeUseToast) => void;
 };
