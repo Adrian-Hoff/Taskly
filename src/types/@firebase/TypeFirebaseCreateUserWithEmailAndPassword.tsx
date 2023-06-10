b5d1d80d1569be1875523ad9cc4d79f3e2669ee8
hykepashi <adrian.hoff2005@gmail.com>
2023-06-10 10:51:16 -0300

    feat: TypeFirebaseCreateUserWithEmailAndPassword/ create this type

--- src/types/@firebase/TypeFirebaseCreateUserWithEmailAndPassword.tsx
+++ src/types/@firebase/TypeFirebaseCreateUserWithEmailAndPassword.tsx
@@ -1,9 +1,5 @@
-//firebase
-import { PhoneAuthCredential } from "firebase/auth";
-
 export type TypeFirebaseCreateUserWithEmailAndPassword = {
   name: string;
   email: string;
   password: string;
-  phoneNumber: PhoneAuthCredential;
 };
