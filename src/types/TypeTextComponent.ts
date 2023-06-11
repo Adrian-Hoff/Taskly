968254ba7f6f3a770843a6a9f3a9994ff0721ed8
hykepashi <adrian.hoff2005@gmail.com>
2023-06-11 15:20:03 -0300

    refactor: TypeTextComponent/ refactor this component

--- src/types/TypeTextComponent.ts
+++ src/types/TypeTextComponent.ts
@@ -1,7 +1,7 @@
 import { ITextProps } from "native-base";
 
 export type TypeTextComponent = ITextProps & {
-  text: string;
+  text: string | undefined;
   color: string;
   fontSize: string;
   fontFamily: string;
