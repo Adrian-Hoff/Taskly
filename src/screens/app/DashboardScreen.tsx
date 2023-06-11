25caa6d020bde5ec2f1cf70fa6bd5a5071c79def
hykepashi <adrian.hoff2005@gmail.com>
2023-06-11 20:47:37 -0300

    feat: DashboardScreen/ init this screen

---
+++ src/screens/app/DashboardScreen.tsx
@@ -0,0 +1,10 @@
+//react
+import React from "react";
+
+//native-base
+import { Box } from "native-base";
+function DashboardScreen() {
+  return <Box flex={1}></Box>;
+}
+
+export default React.memo(DashboardScreen);
