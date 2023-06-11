0fb528743ec84a1e8f8dc943b68dd6b1934e0b52
hykepashi <adrian.hoff2005@gmail.com>
2023-06-11 20:09:50 -0300

    feat: AuthContextProvider/ create this context

---
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -0,0 +1,37 @@
+//react
+import {
+  createContext,
+  useState,
+  ReactNode,
+  useEffect,
+  SetStateAction,
+} from "react";
+
+//types
+import { TypeReactNode } from "../../types/@contexts/TypeReactNode";
+import { TypeAuthContext } from "../../types/@contexts/TypeAuthContext/TypeAuthContext";
+import { TypeAuthContextUser } from "../../types/@contexts/TypeAuthContext/TypeAuthContextUser/TypeAuthContextUser";
+
+export const AuthContext = createContext<TypeAuthContext>(
+  {} as TypeAuthContext
+);
+
+function AuthContextProvider({ children }: TypeReactNode) {
+  const [user, setUser] = useState<TypeAuthContextUser>(
+    {} as TypeAuthContextUser
+  );
+
+  useEffect(() => {
+    console.log(user.displayName);
+    console.log(" ");
+
+    console.log(user);
+  }, [user]);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+export default AuthContextProvider;
