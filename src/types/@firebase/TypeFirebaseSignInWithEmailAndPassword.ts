ae7d0a775aed4ac58183656f8407690af975eaa5
hykepashi <adrian.hoff2005@gmail.com>
2023-06-11 20:06:58 -0300

    refactor: TypeFirebaseSignInWithEmailAndPassword/ add setUser type -> setUser: Dispatch<SetStateAction<TypeAuthContextUser>> | Function;

--- src/types/@firebase/TypeFirebaseSignInWithEmailAndPassword.ts
+++ src/types/@firebase/TypeFirebaseSignInWithEmailAndPassword.ts
@@ -1,4 +1,8 @@
+import { Dispatch, SetStateAction } from "react";
+import { TypeAuthContextUser } from "../@contexts/TypeAuthContext/TypeAuthContextUser/TypeAuthContextUser";
+
 export type TypeFirebaseSignInWithEmailAndPassword = {
   email: string;
   password: string;
+  setUser: Dispatch<SetStateAction<TypeAuthContextUser>> | Function;
 };
