13c97a60c3f6777eeb4bcd4507cf1664930f5f68
hykepashi <adrian.hoff2005@gmail.com>
2023-06-11 21:21:28 -0300

    feat: Router/ add AppRoutes with user verification

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,22 @@
+//react
+import { useContext } from "react";
+
+//react-navigation
 import { NavigationContainer } from "@react-navigation/native";
+
+//routes
 import AuthRoutes from "./auth/auth.routes";
+import AppRoutes from "./app/app.routes";
+
+//contexts
+import { AuthContext } from "../contexts/AuthContext/AuthContext";
 
 export default function Router() {
+  const { user } = useContext(AuthContext);
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user?.uid ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
