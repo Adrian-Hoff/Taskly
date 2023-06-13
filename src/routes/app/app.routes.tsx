273774f73e211054a0eeaf37ccfd8a3594d9aeee
hykepashi <adrian.hoff2005@gmail.com>
2023-06-13 18:08:05 -0300

    feat: AppRoutes/ remove header and style tab

--- src/routes/app/app.routes.tsx
+++ src/routes/app/app.routes.tsx
@@ -1,15 +1,46 @@
 //react-navigation
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+//svg
+import VectorDashboardFillSVG from "../../assets/VectorDashboardFillSVG.svg";
+import VectorDashboardOutlineSVG from "../../assets/VectorDashboardOutlineSVG.svg";
 //screens
 import DashboardScreen from "../../screens/app/DashboardScreen";
+import { Box } from "native-base";
 
 export default function AppRoutes() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="dashboardScreen" component={DashboardScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 70,
+          position: "absolute",
+          left: 10,
+          right: 10,
+          bottom: 10,
+          borderTopWidth: 0,
+          borderRadius: 5,
+          shadowColor: "#808080",
+        },
+        tabBarShowLabel: false,
+      }}
+      sceneContainerStyle={{ backgroundColor: "white" }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <VectorDashboardFillSVG />
+            ) : (
+              <VectorDashboardOutlineSVG />
+            ),
+        }}
+        name="dashboardScreen"
+        component={DashboardScreen}
+      />
     </Tab.Navigator>
   );
 }
