2c0b55a83a1378f97da3523a5cdc964ebcad916f
hykepashi <adrian.hoff2005@gmail.com>
2023-06-13 22:00:10 -0300

    refactor: APP_TEXTS/ add textComponent, dashboardCardComponent and inputComponent into dashboardScreen

--- src/content/app/APP_TEXTS.tsx
+++ src/content/app/APP_TEXTS.tsx
@@ -3,5 +3,30 @@ export const APP_TEXTS = {
     dashboardHeaderComponent: {
       text_1: "HELLO",
     },
+    inputComponent: {
+      text_1: "You can type your task here",
+    },
+    dashboardCardComponent: {
+      text_1:
+        "“Taskly, I need to took my pills every day at 8AM. I need to eat 10 minutes later”",
+      text_2:
+        "“Hey, I’m going to Pam’s house tomorrow night at 8PM, we are going to study math. She lives at 123 Broadway Avenue.”",
+      text_3:
+        "“Hey, I’m going to Pam’s house tomorrow night at 8PM, we are going to study math. She lives at 123 Broadway Avenue.”",
+    },
+    textComponent: {
+      text_1: `1. What will be done: briefly describe the action or activity that will be performed in the task.
+
+2. Date and Time: specify the date and time of the task.
+
+3. Location (optional): include the place where the task will be carried out, if necessary.
+
+4. Repetitions (optional): mention if the task repeats and, if so, provide details about the frequency, if necessary.
+
+5. Notes (optional): provide any additional relevant notes for the task, such as specific instructions or additional details, if necessary.
+   
+6. Fell free to try other informations
+`,
+    },
   },
 };
