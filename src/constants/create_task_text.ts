b3d8c24e837bdbaadf4fc260dc868049800d4847
hykepashi <adrian.hoff2005@gmail.com>
2023-06-17 22:54:38 -0300

    feat: create_task_text/ create this constant

---
+++ src/constants/create_task_text.ts
@@ -0,0 +1,30 @@
+export const create_task_text = `
+Separe as informações do texto a seguir em um objeto JSON contendo 
+o dia da tarefa, o mes da tarefa, um título para a tarefa e o horário. 
+Caso não seja possível responder com: "não foi possivel criar a tarefa". 
+Atenha-se a responder com apenas um obj JSON. Formato do JSON:
+{
+    "dia":"",
+    "mes":"",
+    "horário":"",
+    "titulo":"" 
+}, o texto da tarefa é: 
+`;
+
+export const create_task_text_two = ` O texto é uma tarefa a ser realizada com
+Dia,
+Horario,
+Titulo,
+  o formato da resposta deve vir em JSON entre {}, nao adicione nada a mais que o objeto JSON na resposta. 
+
+      "dia": resposta (padrão:{
+          hoje => 'current_day';
+          amanha => 'next_day';
+          dia do mês => numero em string;
+      }),
+      "mes": se foi mencionado, retorne o numero do mes no calendario, se nao mencionado retorne: 'current_month'
+      "time": horario em formato de tempo: "hh:mm", caso nao informado: "null"
+      "tittle": de um titulo para a tarefa
+  
+
+  texto:`;
