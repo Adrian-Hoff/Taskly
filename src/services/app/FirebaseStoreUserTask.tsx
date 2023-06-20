a262d1f6d07ed1e2eaa8a581216c99c0cdd334c9
hykepashi <adrian.hoff2005@gmail.com>
2023-06-20 17:37:38 -0300

    refactor: FirebaseStoreUserTask

--- src/services/app/FirebaseStoreUserTask.tsx
+++ src/services/app/FirebaseStoreUserTask.tsx
@@ -1,12 +1,17 @@
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { TypeFirebaseStoreUserTask } from "../../types/@firebase/app/TypeFirebaseStoreUserTask";
 
 export default async function FirebaseStoreUserTask({
   user,
   taskJSON,
 }: TypeFirebaseStoreUserTask) {
-  const store = await setDoc(
-    doc(getFirestore(), "users", user.uid, taskJSON.utc, "tasks"),
+  const date = new Date(taskJSON.utc);
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const year = date.getUTCFullYear();
+
+  const store = addDoc(
+    collection(getFirestore(), "tasks", user.uid, `${day}-${month}-${year}`),
     {
       ...taskJSON,
     }
