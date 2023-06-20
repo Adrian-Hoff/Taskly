4d24f8a0b5e54342d77076d597524cdd075d7968
hykepashi <adrian.hoff2005@gmail.com>
2023-06-20 18:53:19 -0300

    feat: TaskCardComponent/ create this component

---
+++ src/components/app/TaskListScreen/TaskCardComponent.tsx
@@ -0,0 +1,62 @@
+//react
+import React, { useEffect } from "react";
+
+//themes
+import { THEMES } from "../../../themes/Themes";
+
+//svg
+import CancelFillSVG from "../../../assets/CancelFillSVG.svg";
+import ConfirmFillSVG from "../../../assets/ConfirmFillSVG.svg";
+
+//types
+import { TypeTaskCardComponent } from "../../../types/@components/TypeTaskCardComponent";
+
+//react-native
+import { TouchableOpacity } from "react-native";
+
+//native-base
+import { Box, HStack, VStack } from "native-base";
+
+//components
+import TextComponent from "../../TextComponent";
+import { AppUtil_DateConverter } from "../../../utils/app/AppUtil_DateConverter";
+
+function TaskCardComponent({ taskJSON }: TypeTaskCardComponent) {
+  const time = AppUtil_DateConverter(taskJSON.utc).GetUTCTime();
+  const utcDbPath = AppUtil_DateConverter(taskJSON.utc).DateFormats().dbPath;
+
+  return (
+    <Box>
+      <TouchableOpacity>
+        <HStack py={3}>
+          <VStack space={1}>
+            <TextComponent
+              fontFamily={THEMES.fontFamily.Lato_700Bold}
+              color={THEMES.color.font.gray40}
+              fontSize={THEMES.fontSize.lg}
+              text={taskJSON.title}
+            />
+            <TextComponent
+              fontFamily={THEMES.fontFamily.Lato_400Regular}
+              color={THEMES.color.font.gray60}
+              fontSize={THEMES.fontSize.sm}
+              text={time}
+            />
+          </VStack>
+          <Box flex={1} />
+
+          <HStack space={2} alignItems={"center"}>
+            <TouchableOpacity>
+              <ConfirmFillSVG width={28} height={28} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => console.log("a")}>
+              <CancelFillSVG width={28} height={28} />
+            </TouchableOpacity>
+          </HStack>
+        </HStack>
+      </TouchableOpacity>
+    </Box>
+  );
+}
+
+export default React.memo(TaskCardComponent);
