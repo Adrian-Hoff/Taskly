ae5d1865ba451b20aaa46263a7696212dd829e1a
hykepashi <adrian.hoff2005@gmail.com>
2023-06-23 19:36:37 -0300

    feat: CurrentTaskContextProvider/ create this context

---
+++ src/contexts/app/CurrentTaskContext.tsx
@@ -0,0 +1,26 @@
+//react
+import { createContext, useEffect, useState } from "react";
+
+//types
+import { TypeReactNode } from "../../types/@contexts/TypeReactNode";
+import { TypeStateTaskFromSectionInDATA } from "../../types/@states/app/TaskScreen/TypeStateDATA";
+import { TypeCurrentTaskContext } from "../../types/@contexts/TypeCurrentTaskContext/TypeCurrentTaskContext";
+
+export const CurrentTaskContext = createContext({} as TypeCurrentTaskContext);
+export default function CurrentTaskContextProvider({
+  children,
+}: TypeReactNode) {
+  const [currentTask, setCurrentTask] =
+    useState<TypeStateTaskFromSectionInDATA>(
+      {} as TypeStateTaskFromSectionInDATA
+    );
+
+  useEffect(() => {
+    console.log(currentTask);
+  }, [currentTask]);
+  return (
+    <CurrentTaskContext.Provider value={{ currentTask, setCurrentTask }}>
+      {children}
+    </CurrentTaskContext.Provider>
+  );
+}
